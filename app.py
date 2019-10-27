e9dce21fb2dd04d0534b02138151e10c3002808e
jhaseon <45016912+jhaseon@users.noreply.github.com>
2019-10-27 18:26:51 -0400

    Update app.py
    removed password

--- app.py
+++ app.py
@@ -1,65 +1,56 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_table
+import dash_html_components as html
+from sqlalchemy import create_engine
+import pymysql
 import pandas as pd
+import dash_core_components as dcc
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/jhaseon/Sooryen/master/craigslist.csv')
+db_connection_str = 'mysql+pymysql://root:"password"@localhost/craigslist'
+db_connection = create_engine(db_connection_str)
 
+df = pd.read_sql('SELECT * FROM books_mags', con=db_connection).drop(['id'], axis=1)
 df[' index'] = range(1, len(df) + 1)
 
 app = dash.Dash(__name__)
-
+server = app.server
 PAGE_SIZE = 5
-
 app.layout = html.Div([
     dash_table.DataTable(
-        id='datatable-paging-page-count',
+        id='datatable-filtering-fe',
         columns=[
-            {"name": i, "id": i} for i in sorted(df.columns)
+            {"name": i, "id": i, "deletable": True} for i in df.columns
         ],
-        page_current=0,
-        page_size=PAGE_SIZE,
-        page_action='custom'
+        style_cell_conditional=[
+        {'if': {'column_id': 'titles'},
+         'width': '30%'}],
+        data=df.to_dict('records'),
+        filter_action="native",
+        page_size=PAGE_SIZE
     ),
-    html.Br(),
-    dcc.Checklist(
-        id='datatable-use-page-count',
-        options=[
-            {'label': 'Use page_count', 'value': 'True'}
-        ],
-        value=['True']
-    ),
-    'Page count: ',
-    dcc.Input(
-        id='datatable-page-count',
-        type='number',
-        min=1,
-        max=29,
-        value=20
-    )
+    html.Div(id='datatable-filter-container')
 ])
 
 
 @app.callback(
-    Output('datatable-paging-page-count', 'data'),
-    [Input('datatable-paging-page-count', "page_current"),
-     Input('datatable-paging-page-count', "page_size")])
-def update_table(page_current,page_size):
-    return df.iloc[
-        page_current*page_size:(page_current+ 1)*page_size
-    ].to_dict('records')
+    Output('datatable-filter-container', "children"),
+    [Input('datatable-filtering-fe', "data")])
+def update_graph(rows):
+    if rows is None:
+        dff = df
+    else:
+        dff = pd.DataFrame(rows)
 
-@app.callback(
-    Output('datatable-paging-page-count', 'page_count'),
-    [Input('datatable-use-page-count', 'value'),
-     Input('datatable-page-count', 'value')])
-def update_table(use_page_count, page_count_value):
-    if len(use_page_count) == 0 or page_count_value is None:
-        return None
-    return page_count_value
+    return html.Div()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+    
+
+#References 
+#https://dash.plot.ly/datatable/callbacks
+#https://dash.plot.ly/datatable/filtering
